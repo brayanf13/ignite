0d4e9ec4e8002b1d7d3ac359d0b11206a7abcad8
brayanf13 <brayan.amf@gmail.com>
2022-04-24 00:10:22 -0300

    Usando TypeScript

--- react.js/01-github-explorer/src/components/RepositoryList.tsx
+++ react.js/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,17 +3,23 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+type Repository = {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     function loadRepositories() {
 
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
-        .then(response => response.json())
-        .then(data => setRepositories(data))
+            .then(response => response.json())
+            .then(data => setRepositories(data))
     }, [])
 
     return (
@@ -22,7 +28,7 @@ export function RepositoryList() {
 
             <ul>
                 {repositories.map(repository => {
-                return (<RepositoryItem key={repository.name} repository={repository} />)
+                    return (<RepositoryItem key={repository.name} repository={repository} />)
                 })}
             </ul>
         </section>
